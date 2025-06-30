48f3db3c69c893d2ee98b4b3b9289ba9a08cbb0c
Luis Herrera <luchoherrerafernandez@gmail.com>
2025-06-30 19:35:34 -0300

    up

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -27,7 +27,7 @@ export class CloudinaryService {
     {
       overlay: `text:Arial_45:${encodeURIComponent('id-' +propertyCode)},co_white`,
       gravity: 'south',
-      y: 95,
+      y: 105,
       x: -520,
       opacity: 90,
     },
